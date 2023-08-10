2672cc07165432a590b51195524788e6ccacf63e
Neeraj Kumar Singh <neeraj@localhost>
2023-08-10 12:23:20 +0530

    remove TodoItem comp as it is sub-comp for DisplayTodoItems comp

--- week-4/01-easy-todo-app/src/components/DisplayTodoItems/index.jsx
+++ week-4/01-easy-todo-app/src/components/DisplayTodoItems/index.jsx
@@ -1,5 +1,26 @@
-import TodoItem from "../TodoItem/index.jsx"
-import "../TodoItem/index.css"
+function TodoItem(props) {
+  return (
+    <div id="todoItem-container" data-id={props.todo.id}>
+      <div className="ele-center" id="comp-input-container">
+        <input
+          type="checkbox"
+          id="comp-input"
+          onChange={() => props.markTodoComplete(props.todo.id)}
+        />
+      </div>
+      <div id="todoInfo-container">
+        <div id="todoTitle-container">{props.todo.title}</div>
+        <div id="todoDesc-container">{props.todo.description}</div>
+      </div>
+      <div className="ele-center" id="delTodo-container">
+        <i
+          className="fa-solid fa-trash fa-lg"
+          onClick={() => props.deleteTodo(props.todo.id)}
+        ></i>
+      </div>
+    </div>
+  );
+}
 
 export default function DisplayTodoItems(props) {
     return (
